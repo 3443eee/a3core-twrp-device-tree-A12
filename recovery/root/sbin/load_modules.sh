b850b4d4c45c17206a07f31f28c03661826e2518
Sheff <sheffthechef@proton.me>
2025-09-22 12:46:16 +0300

    Removing (not-working) touch fix
    Removing the old touch fix to make a new one from the start.

--- recovery/root/sbin/load_modules.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-# Loop through all .ko files in socko and insmod them
-for module in /mnt/socko/*.ko; do
-    if [ -f "$module" ]; then
-        echo "Loading module: $module"
-        insmod "$module" || echo "Failed to load: $module"
-    fi
-done
