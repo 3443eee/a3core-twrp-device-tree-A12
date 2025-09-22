995502c576f9960b4830ed530800fad1dba502bf
3443eee <3443p@proton.me>
2025-09-22 23:31:01 +0100

    arm build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,22 +9,23 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 TARGET_USES_UEFI := true
 
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+# Architecture (a64 / arm32_binder64)
+TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a55
 TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
+# Second arch is 64-bit, but only for binder
+TARGET_2ND_ARCH := arm64
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := arm64-v8a
+TARGET_2ND_CPU_ABI2 :=
 TARGET_2ND_CPU_VARIANT := cortex-a55
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Binder interface is 32-bit
 TARGET_USES_64_BIT_BINDER := true
 
 TARGET_CPU_SMP := true
