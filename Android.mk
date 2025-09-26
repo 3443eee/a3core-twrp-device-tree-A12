0d6c0f7c759a0fa8efa073aa02e24a8236f86bc2
3443eee <3443p@proton.me>
2025-09-27 00:28:19 +0100

    try 2

--- Android.mk
+++ Android.mk
@@ -5,19 +5,57 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 
 ifneq ($(filter a3core, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+# =========================
+# Prebuilt vendor libraries
+# =========================
+define prebuilt-shared-lib
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(notdir $(1))
+LOCAL_SRC_FILES := vendor/lib/$(1)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib
+LOCAL_UNSTRIPPED_PATH := $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib
+include $(BUILD_PREBUILT)
+endef
+
+PREBUILT_LIBS := \
+    libtrusty.so \
+    libtrustyHalHelper.so \
+    libkeymaster4.so \
+    libkeymaster41.so \
+    libkeymaster4support.so \
+    libkeymaster4_1support.so \
+    libkeymaster_portable.so \
+    libkeymaster_messages.so \
+    libnvitem_crypto.so
+
+$(foreach lib,$(PREBUILT_LIBS),$(eval $(call prebuilt-shared-lib,$(lib))))
+
+# =========================
+# Prebuilt vendor binaries
+# =========================
+define prebuilt-exec-bin
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(notdir $(1))
+LOCAL_SRC_FILES := vendor/bin/$(1)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/vendor/bin
+include $(BUILD_PREBUILT)
+endef
+
+PREBUILT_BINS := \
+    vold \
+    wait_for_keymaster
+
+$(foreach bin,$(PREBUILT_BINS),$(eval $(call prebuilt-exec-bin,$(bin))))
 
 endif
